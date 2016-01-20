c7c88a7b2930c9a88d726ebb91ec3e97809940de
Ronald22 <ronald22castro@gmail.com>
2016-01-20 03:16:59 -0500

    ProyectoWeb 0.8

--- public/js/inicioCl.js
+++ public/js/inicioCl.js
@@ -23,29 +23,58 @@ $(document).ready(function(){
     $("#desplegable").css({ display: 'none' });
     
     $('#btniniciosecion').click(function(){                 
-        var user = $('#txtnombre').val();
-        var contra = $('#txtcontra').val(); 
-        var usuario = "Juan";
-        var contrusua = "12345";
 
-        if((user === usuario)&&(contra === contrusua)){
-            alert('Bienvenido a Terra');
-              window.location.href='pagCliente/inicio.html';
-        }
-        else{
-            if(user===usuario){
-                alert('contraseña incorrecta');
-            }
-            if (contra===contrusua){
-                alert('correo incorrecto');
+        $.ajax({
+            type:"GET",
+            url:"http://localhost:3000/terra",
+            dataType:"json",
+            contentType:"text/plain"
+        }).done(function(msg){
+            var user = $('#txtnombre').val();
+            var contra = $('#txtcontra').val(); 
+            var usuario = msg.usuario; 
+            var contrausua = msg.contraseña;
+            
+            if((user === usuario)&&(contra === contrausua)){
+                alert('Bienvenido a Terra');
+                  window.location.href='pagCliente/inicio.html';
             }
             else{
-                if((user!==usuario)&&(contra!==contrusua)){
-                    alert('ingreso de datos incorrectos');
+                if(user===usuario){
+                    alert('contraseña incorrecta');
                 }
+                if (contra===contrausua){
+                    alert('usuario incorrecto');
+                }
+                else{
+                    if((user!==usuario)&&(contra!==contrausua)){
+                        alert('ingreso de datos incorrectos');
+                    }
 
-            } 
-        }
+                } 
+            }
+        });        
     });
+    
+    $("#btnregistro").click(function(){
+                
+                var usuario =$("#txtusuario").val();
+                var email=$("#txtcorreo").val();
+                var contraseña=$("#txtcontra").val();
+                var fechanacimiento=$("#txtnacimiento").val();
+                datos={"apellidos":"","contraseña":contraseña,"edad":"","email":email,"fechanacimiento":fechanacimiento,
+                       "fecharegistro":"","foto":"","nombres":"","usuario":usuario};
+                
+               $.ajax({
+                   type:"POST",
+                   url:"http://localhost:3000/terra",
+                   dataType:"text",
+                   contentType:"application/json",
+                   data: JSON.stringify(datos)
+               }).done(function(msg){
+                   alert(msg);
+               }); 
+           
+            });
     
 });
--- servUsuario.js
+++ servUsuario.js
@@ -2,40 +2,43 @@
 const express = require('express')
 const http = require('http')
 const bodyParser=require('body-parser')
+const Firebase= require('firebase')
 let items=[]
+let contador=0
 
+
+let miRuta= new Firebase('https://terra-4x4.firebaseio.com/Usuario')
 let router=express.Router()
 router.use(bodyParser())
 router.route('/')
 .get(function(request,response){
-    response.status(200).send(items)
+    items=[]
+    miRuta.once("value",function(snap){
+        let nuevoUsuario=snap.val()
+        items.push(nuevoUsuario)
+        console.log(contador++)
+        return response.send(items)
+     })
 })
 .post(function(req,res,next){
-    items.push(req.body)
-    res.status(200).send('post ESTA FUNCIONANDO')
+    miRuta.child(req.body.usuario).set(req.body)
+    res.status(200).send('ok')
 })
 .put(function(req,res,next){
 
-    for (var i=0;i<items.length;i++)
-    {
-         if (items[i]['id']==req.body.id )
-        {
-            items[i]['nombre']=req.body.nombre
-            items[i]['apellido']=req.body.apellido
-            items[i]['fechaNa']=req.body.fechaNa
-            items[i]['email']=req.body.email
-        }
-    }
+    miRuta.child(req.body.usuario).set(req.body)
      res.status(200).send(req.body.codigo)
 })
 .delete(function(req,res,next){
-    items= items.filter(function(elemento){
-         return elemento.id!=req.body.id
-    })
-    res.status(200).send('delete esta funcionando')
+    miRuta.child(req.body.usuario).remove(function(error){
+        if (error)
+         {
+             return res.status(404).send('error ')
+         }
+     })
+    return res.status(200).send('ok')
 });
 
 let app= express()
-.use('/todo',router)
+.use('/terra',router)
 .listen(3000)
-
