789c3d89b8e94d76dac05503412f8d5a00f4af4a
Ronald22 <ronald22castro@gmail.com>
2016-02-05 01:27:27 -0500

    ProyectoWeb 2.6

--- server.js
+++ server.js
@@ -4,43 +4,11 @@ const http = require('http')
 const port = process.env.PORT || 8080;
 const bodyParser=require('body-parser')
 const Firebase= require('firebase')
-let items=[]
-let contador=0
 
+var claseUsuario = require('./Usuario')
 
-let miRuta= new Firebase('https://terra-4x4.firebaseio.com/Usuario')
-let router=express.Router()
-router.use(bodyParser())
-router.route('/')
-.get(function(request,response){
-    items=[]
-    miRuta.once("value",function(snap){
-        let nuevoUsuario=snap.val()
-        items.push(nuevoUsuario)
-        console.log(contador++)
-        return response.send(items)
-     })
-})
-.post(function(req,res,next){
-    miRuta.child(req.body.usuario).set(req.body)
-    res.status(200).send('Se registro correctamente')
-})
-.put(function(req,res,next){
-
-    miRuta.child(req.body.usuario).set(req.body)
-    res.status(200).send(req.body.codigo)
-})
-.delete(function(req,res,next){
-    miRuta.child(req.body.usuario).remove(function(error){
-        if (error)
-        {
-            return res.status(404).send('error ')
-        }
-    })
-    return res.status(200).send('ok')
-});
+claseUsuario.Usuario;
 
 let app = express()
-.use('/usuario',router)
 .use(express.static(__dirname+'/public'))
 .listen(port)
